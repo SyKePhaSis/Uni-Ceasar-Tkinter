a0676111147f4ec036b671bae80dc9c145f22fb5
SyKePhaSis <leojr.crassaris@hotmail.com>
2023-11-30 21:02:21 +0200

    (Feat): Created Encryption and Decryption

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+UPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
 
+def encrypt(userInput,shift):
+    encrypted = ""
+    for i in userInput:
+        if i.isupper() == True:
+            index = UPERCASE.find(i)
+            encrypted += UPERCASE[index + shift]
+        if i.islower() == True:
+            index = LOWERCASE.find(i)
+            encrypted += LOWERCASE[index + shift]
+    return encrypted
+
+def decrypt(userInput, shift):
+    decrypted = ""
+    for i in userInput:
+        if i.isupper():
+            index = UPERCASE.find(i)
+            decrypted += UPERCASE[index - shift]
+        if i.islower():
+            index = LOWERCASE.find(i)
+            decrypted += LOWERCASE[index - shift]
+    return decrypted
+            
 def main():
-    print("Working")
-
+    encrypted = encrypt("test", 2)
+    decrypted = decrypt("vguv",2)
+    print(encrypted," ",decrypted)
+    
 if __name__ == "__main__":
     main()
